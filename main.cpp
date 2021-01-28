e63fa204c34820c5968a6629be923b4dc05ecb65
Himanshu Lohaniwal <lohaniwalhimanshu@gmail.com>
2021-01-28 16:12:40 +0530

    Learned New Things

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int          
+main ()
+{
+  int n, a;
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    {
+        for (int j = n;j>i;j--)
+        cout<<" ";
+        for(int k = 1 ; k<=i;k++)
+        cout<<"*";
+        for(int k = 2 ; k<=i;k++)
+        cout<<"*";
+        cout<<endl;
+    }
+for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1;j<=i;j++)
+        cout<<" ";
+        for(int k = n ; k>i;k--)
+        cout<<"*";
+        for(int k = n-2 ; k>=i;k--)
+        cout<<"*";
+        cout<<endl;
+    }
+
+  return 0;
+}
